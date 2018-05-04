88d215354bc8a145f19d9f573471b4306e3de696
Scott Olsen <scg.olsen@gmail.com>
2018-05-04 18:05:09 -0400

    Update escaping functions

--- src/json_pointer/escape.clj
+++ src/json_pointer/escape.clj
@@ -1,17 +1,25 @@
 (ns json-pointer.escape 
-  ""
+  "Escape and unescape JSON pointer strings."
   (:require [clojure.string :as s]))
 
 (defn unescape 
-  "Unescape strings."
+  "Unescape JSON pointer strings."
   [token]
   (case (last token)
         "0" "~"
         "1" "/"))
 
 (defn escape 
-  "Escape invalid characters."
-  [pointerstring]
-  (-> pointerstring 
+  "Escape invalid JSON pointer characters."
+  [string]
+  (-> string
       (s/replace #"~" "~0")
       (s/replace #"/" "~1")))
+
+(defn ->escaped-string 
+  "Takes a value and converts it to an escaped JSON pointer string."
+  [k]
+  (cond (keyword? k) (escape (s/replace-first (str k) #":" ""))
+        (string? k) (escape k)
+        :else k))
+
