49c13d93590fd95adb79cd0ea536d6b007b42b95
Scott Olsen <scg.olsen@gmail.com>
2018-05-08 20:02:33 -0400

    Hardcode abnf in parser

--- src/json_pointer/parser.clj
+++ src/json_pointer/parser.clj
@@ -2,7 +2,11 @@
   "A parser for json pointers."
   (:require [instaparse.core :as i]))
 
-(def parser (i/parser "src/json_pointer/data/abnf.txt" :input-format :abnf))
+(def parser (i/parser "json-pointer = *1(\"#\" (reference-token / \"\" / array-index)) *( \"/\" (reference-token / \"\" / array-index) ) / \"\"
+reference-token = *( unescaped / escaped ) 
+unescaped = *( *%x00-2E / *%x30-7D / *%x7F-10FFFF ) 
+escaped = \"~\" ( \"0\" / \"1\" )
+array-index = %x30 / ( %x31-39 *(%x30-39) )" :input-format :abnf))
 
 (defn parse [pointer]
   "Parse a JSON pointer."
